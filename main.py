5584f5ef946f155207ed600df0cb3a25ca373b6a
Makar Hysko <hysko.mm@gmail.com>
2024-04-30 21:06:36 +0300

    Add a window with button

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+from PySide6.QtWidgets import QApplication, QPushButton
+
+app = QApplication([])
+button = QPushButton("Привіт, світ!")
+button.resize(300, 200)
+
+button.show()
+app.exec()
